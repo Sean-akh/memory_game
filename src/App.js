c9bebf780842e117298a023513307a3defd59840
Sean-Akh <sean.akhlaghi@gmail.com>
2018-07-17 12:05:51 -0700

    updated the app.js, added images and modified the css files.

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import './App.css';
+import imageArray from './components/images';
+
+//
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      images: imageArray,
+      message: "To Start, Please Select an Image!",
+      score: 0,
+      topScore: 0
+    }
+  }
+
+  //================================================================================
+topScore = () => {
+  let scoreJam = this.state.score;
+  let topscoreJam = this.state.topScore;
+console.log('the score is at topScore section: ', scoreJam);
+console.log('topscore at the top: ', topscoreJam)
+  if ( scoreJam > topscoreJam) {
+      this.setState({topScore: scoreJam})
+  }
+  // console.log('Top Score is: ', this.topScore);
+}
+
+//================================================================================
+//randomize the order of images
+ imageRandomizer = () => {
+  for(let i = 0; i < imageArray.length; i++) {
+      let j = i + Math.floor(Math.random() * (imageArray.length - i));
+          let newImageArray = imageArray[j];
+          imageArray[j] = imageArray[i];
+          imageArray[i] = newImageArray;
+  }
+}
+
+//================================================================================
+ imageHandler = (id) => {
+  let imgChoice = this
+  //after click loop through imageArray and change the click state, increase score 
+  for (let i = 0; i < imageArray.length; i++) {
+      if (id===imageArray[i].id) {
+          if (imageArray[i].clicked === false) {
+              imageArray[i].clicked = true;
+              //let scoreNew = this.state.score;
+              this.setState({
+                  message: "You Guessed Correctly!!!",
+                  score: ++this.state.score 
+              })
+              this.topScore();
+              setTimeout(() => {
+                  imgChoice.setState({
+                      message: 'Please Select Another Image!'
+                  })
+              },1500)
+          }
+          //or let the palyer know he lost!
+          else{
+              this.setState({
+                  message: 'You Have Guessed incorrectly!',
+                  score: 0
+              })
+              // reset the clicked state to false to start the game
+              imageArray.map(image => image.clicked = false);
+          }
+      }
+  }
+  //rearrange the image grid
+  this.imageRandomizer();
+}
+//================================================================================
+
+
+  render() { 
+    //create iage grid and make it clickable
+        let showImages = this.state.images.map((image, index) =>
+          <div className="image-run">
+            <a href="#" key={index} onClick={() => this.imageHandler(image.id)}>
+            <img src = {image.url} alt="" className ="img-one"></img>
+            </a>
+          </div>
+        )
+
+    //============================== show everything ====================================
+    return (
+      <div className="App">
+        <header className="header">
+          <div className="spacer"></div>
+          <div><p className="title">Memory Game</p> </div>
+          <div className="spacer"></div>
+          <div className="spacer"></div>
+          <div className="message"> 
+             {this.state.message}
+          </div>
+          <div>
+// {console.log('the score at the bottom is: ', this.state.score, '\nthe top score is:', this.state.topScore, '\n==============')}
+            <span className = "score"> Score: </span> <span className = "top-score">{this.state.score}</span>
+            <span className = "score-line"> | </span> 
+            <span className = "score">Top Score: </span> <span className = "top-score">{this.state.topScore} </span>
+          </div>
+        </header>
+        <div className="img-grid"> {showImages} </div>
+      </div>
+    );
+  }
+}
+  
+
+export default App;
---
+++ src/components/images.js
@@ -0,0 +1,206 @@
+//import Image list
+import image1 from "../img/img01.jpg"
+import image2 from "../img/img02.jpg"
+import image3 from "../img/img03.jpg"
+import image4 from "../img/img04.jpg"
+import image5 from "../img/img05.jpg"
+import image6 from "../img/img06.jpg"
+import image7 from "../img/img07.jpg"
+import image8 from "../img/img08.jpg"
+import image9 from "../img/img09.jpg"
+import image10 from "../img/img10.jpg"
+import image11 from "../img/img11.jpg"
+import image12 from "../img/img12.jpg"
+import image13 from "../img/img13.jpg"
+import image14 from "../img/img14.jpg"
+import image15 from "../img/img15.jpg"
+import image16 from "../img/img16.jpg"
+import image17 from "../img/img17.jpg"
+import image18 from "../img/img18.jpg"
+import image19 from "../img/img19.jpg"
+import image20 from "../img/img20.jpg"
+import image21 from "../img/img21.jpg"
+import image22 from "../img/img22.jpg"
+import image23 from "../img/img23.jpg"
+import image24 from "../img/img24.jpg"
+import image25 from "../img/img25.jpg"
+import image26 from "../img/img26.jpg"
+import image27 from "../img/img27.jpg"
+import image28 from "../img/img28.jpg"
+import image29 from "../img/img29.jpg"
+import image30 from "../img/img30.jpg"
+import image31 from "../img/img31.jpg"
+import image32 from "../img/img32.jpg"
+import image33 from "../img/img33.jpg"
+
+//create an array from the images and assign id and if the image been clicked to false
+let imageArray =[
+    {
+        id: 1,
+        url: image1,
+        clicked: false
+    },
+    {
+        id: 2,
+        url: image2,
+        clicked: false
+    },
+    {
+        id: 3,
+        url: image3,
+        clicked: false
+    },
+    {
+        id: 4,
+        url: image4,
+        clicked: false
+    },
+    {
+        id: 32,
+        url: image32,
+        clicked: false
+    },
+    {
+        id: 5,
+        url: image5,
+        clicked: false
+    },
+    {
+        id: 30,
+        url: image30,
+        clicked: false
+    },
+    {
+        id: 6,
+        url: image6,
+        clicked: false
+    },
+    {
+        id: 7,
+        url: image7,
+        clicked: false
+    },
+    {
+        id: 8,
+        url: image8,
+        clicked: false
+    },
+    {
+        id: 31,
+        url: image31,
+        clicked: false
+    },
+    {
+        id: 9,
+        url: image9,
+        clicked: false
+    },
+    {
+        id: 10,
+        url: image10,
+        clicked: false
+    },
+    {
+        id: 11,
+        url: image11,
+        clicked: false
+    },
+    {
+        id: 12,
+        url: image12,
+        clicked: false
+    },
+    {
+        id: 13,
+        url: image13,
+        clicked: false
+    },
+    {
+        id: 26,
+        url: image26,
+        clicked: false
+    },
+    {
+        id: 27,
+        url: image27,
+        clicked: false
+    },
+    {
+        id: 14,
+        url: image14,
+        clicked: false
+    },
+    {
+        id: 15,
+        url: image15,
+        clicked: false
+    },
+    {
+        id: 16,
+        url: image16,
+        clicked: false
+    },
+    {
+        id: 17,
+        url: image17,
+        clicked: false
+    },
+    {
+        id: 18,
+        url: image18,
+        clicked: false
+    },
+    {
+        id: 19,
+        url: image19,
+        clicked: false
+    },
+    {
+        id: 20,
+        url: image20,
+        clicked: false
+    },
+    {
+        id: 21,
+        url: image21,
+        clicked: false
+    },
+    {
+        id: 22,
+        url: image22,
+        clicked: false
+    },
+    {
+        id: 23,
+        url: image23,
+        clicked: false
+    },
+    {
+        id: 24,
+        url: image24,
+        clicked: false
+    },
+    {
+        id: 25,
+        url: image25,
+        clicked: false
+    },
+    {
+        id: 28,
+        url: image28,
+        clicked: false
+    },
+    {
+        id: 29,
+        url: image29,
+        clicked: false
+    },
+    {
+        id: 33,
+        url: image33,
+        clicked: false
+    }
+////////////// end of Array    
+];
+
+export default imageArray;
